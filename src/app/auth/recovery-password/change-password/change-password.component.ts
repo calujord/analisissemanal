161f82be08f4d4641e6654f2e71762fb868d0a62
christian <christian.valverde@nubesoft.ec>
2021-05-22 12:14:35 -0500

    formulario de cambio de password

--- src/app/auth/recovery-password/change-password/change-password.component.ts
+++ src/app/auth/recovery-password/change-password/change-password.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.sass']
+  styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+  formGroup!:FormGroup;
 
+  
   ngOnInit(): void {
+    this.formGroup=this.fb.group({
+      usuario:['',[Validators.required,Validators.email]],
+      contrasenaActual:['',[Validators.required,Validators.minLength(3)]],
+      nuevaContrasena:['',[Validators.required,Validators.minLength(3)]],
+      validacionContrasena:['',[Validators.required,Validators.minLength(3)]]
+    },{ validators: this.identityRevealedValidator });
   }
+  
+ identityRevealedValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const validacionContrasena = control.get('validacionContrasena');
+  const nuevaContrasena = control.get('nuevaContrasena');
 
+  return validacionContrasena.value != nuevaContrasena.value ? { coincidencia: true } : null;
+};
+ onActualizarContrasenas(){
+
+ }
 }
