1a1475aafe928733971f87cd63ab86c7720deda3
christian <christian.valverde@nubesoft.ec>
2021-05-22 12:29:41 -0500

    formulario de recuperación de password

--- src/app/auth/recovery-password/request-recovery-password/request-recovery-password.component.ts
+++ src/app/auth/recovery-password/request-recovery-password/request-recovery-password.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-request-recovery-password',
   templateUrl: './request-recovery-password.component.html',
-  styleUrls: ['./request-recovery-password.component.sass']
+  styleUrls: ['./request-recovery-password.component.css']
 })
 export class RequestRecoveryPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+  formGroup!:FormGroup;
 
   ngOnInit(): void {
+    this.formGroup=this.fb.group({
+      usuario:['',[Validators.required,Validators.email]],
+    });
   }
-
 }
